323690148d794a9ba52f6c48e182661a254bd6b0
MiguelMateoCollado <miguizindex@gmail.com>
2022-08-07 09:20:32 -0400

    Mi segundo intento no lo hice aquella vez porque era un inseguro pero ahora estoy mas que listo recordare mi vida y mis palabras y lo que quiero para ella para lograr lo que me proponga hoy empeze a luchar por mi libertad

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -1,7 +1,7 @@
 // En estas primeras 6 preguntas, reemplaza `null` por la respuesta
 
 // Crea una variable "string", puede contener lo que quieras:
-const nuevaString = null;
+const nuevaString = "Hoy decidi cambiar mi vida en un 180c y necesito de mi propia voluntad para lograrlo je si solo depende de eso pues estoy mas que listo";
 
 // Crea una variable numérica, puede ser cualquier número:
 const nuevoNum = null;
